7622569acb792b5055b4b182f145820c4ab9536b
rodebertx <rh-21@web.de>
2022-03-13 09:31:43 +0100

    added manifest options! important !!! #doc

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
   pwa: {
+    assetsVersion: "0.1.0",
     themeColor: "#ffedb8",
     iconPaths: {
       faviconSVG: "img/icons/favicon.svg",
@@ -11,6 +12,31 @@ module.exports = defineConfig({
       appleTouchIcon: "img/icons/ios/152.png",
       maskIcon: "img/icons/android/android-launchericon-512-512.png",
       msTileImage: "img/icons/windows11/LargeTile.scale-400.png",
+    },
+    manifestOptions: {
+      name: "Bienenzucht W.Durek",
+      short_name: "Duris Bienchen",
+      theme_color: "#ffedb8",
+      icons: [
+        {
+          src: "./favicon.svg",
+          sizes: "512x512",
+          type: "image/svg+xml",
+          purpose: "any maskable",
+        },
+        {
+          src: "./img/icons/android-launchericon-192-192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: "./img/icons/android-launchericon-512-512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable",
+        },
+      ],
     },
   },
 });
